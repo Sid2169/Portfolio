37e300503c7fb152c3bdba8ae140e2deaa4c90bb
Sid2169 <siddharthasingh21109572@gmail.com>
2025-12-24 13:32:42 +0530

    chore: temporarily comment out LogoSection from App
    - Comment out LogoSection import and usage in App.jsx
    - Temporarily remove logo showcase from main layout
    - Keep component ready for future integration when styling/positioning is finalized
    - Maintain NavBar, Hero, and ShowcaseSection as primary sections
    - Allow continued development of other portfolio sections without visual interference

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import LogoSection from "./sections/LogoSection"
+// import LogoSection from "./sections/LogoSection"
 import NavBar from "./components/NavBar"
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
@@ -9,7 +9,7 @@ const App = () => {
       <NavBar />
       <Hero/>
       <ShowcaseSection />
-      <LogoSection />
+      {/* <LogoSection /> */}
     </>
   )
 }
